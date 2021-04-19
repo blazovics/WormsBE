7b0b4cd46bde0cb0e71f59d3bac8ac862fc09d7a
Sárosi Fanni <fannisarosi457@gmail.com>
2021-04-19 17:21:28 +0200

    MovementStart

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public float horizontalMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    void FixedUpdate()
+    {
+        //Move character
+        controller.Move(horizontalMove, false);
+    }
+}
