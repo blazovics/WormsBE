905d98a13f33a6f35bbfb07116e6523f59fe1785
Zsomac <clashwithzsomac@gmail.com>
2021-05-21 07:35:11 +0200

    plusz zene

---
+++ Assets/Scripts/SoundTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTracker : MonoBehaviour
+{
+    public AudioSource[] soundFX;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            soundFX[0].Play();
+        }
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            soundFX[1].Play();
+        }
+    }
+}
